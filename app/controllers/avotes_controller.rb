87fa7bb7b1908afcfe1dc70cc71c3a6b274ee787
manuelbb <manubustos@outlook.com.ar>
2017-11-28 17:07:24 -0300

    Agragados los permisos

--- app/controllers/avotes_controller.rb
+++ app/controllers/avotes_controller.rb
@@ -1,7 +1,7 @@
 class AvotesController < ApplicationController
   def new
-    @q = Question.find(params[:question_id])    
     @a = Answer.find(params[:answer_id])
+    @q = Question.find(@a.question_id)    
     @u = User.find(params[:user_id])
     @vote = Avote.new
     @vote.user_id = current_user.id
@@ -23,18 +23,19 @@ class AvotesController < ApplicationController
   end
 
   def destroy
-    @q = Question.find(params[:question_id])    
-    @a = Answer.find(params[:answer_id])    
+    @a = Answer.find(params[:answer_id])
+    @q = Question.find(@a.question_id)
     @u = User.find(params[:user_id])
     @vote = Avote.where(:user_id => current_user.id, :answer_id => @a.id, :positivo => params[:positivo])
+    if @vote.first.positivo == true
+      @a.decrement!(:positive_votes, 1)
+      User.update(params[:user_id], :puntos => @u.puntos - 10)
+    else
+      @a.decrement!(:negative_votes, 1)
+      User.update(params[:user_id], :puntos => @u.puntos + 2)
+    end
+
     if @vote.destroy_all
-      if @vote.positivo == true
-        @a.decrement!(:positive_votes, 1)
-        User.update(params[:user_id], :puntos => @u.puntos - 10)
-      else
-        @a.decrement!(:negative_votes, 1)
-        User.update(params[:user_id], :puntos => @u.puntos + 2)
-      end
       redirect_to @q, notice: "Se quito el voto."
     else
       redirect_to @q, notice: "Hubo un error al borrar el voto."
